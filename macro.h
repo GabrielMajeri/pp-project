6c18df05297e4d57b49db2f0f9c4f49405311c64
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-11-30 20:54:36 +0200

    Add more useful macros and typedefs

--- macro.h
+++ macro.h
@@ -6,4 +6,16 @@
 /// Nr. de elemente al unui vector
 #define ARRAY_LEN(arr) (sizeof(arr) / sizeof(arr[0]))
 
+/// Returneaza minimul dintre doua valori
+#define MIN(a, b) \
+    ((a) > (b) ? (a) : (b))
+
+/// Returneaza maximul dintre doua valori
+#define MAX(a, b) \
+    ((a) < (b) ? (a) : (b))
+
+/// Asigura ca x-ul este in intervalul [min, max]
+#define CLAMP(x, min, max) \
+    MIN(max, MAX(min, x))
+
 #endif
